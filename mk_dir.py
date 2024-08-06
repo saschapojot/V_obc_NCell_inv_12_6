194e522222e1ac37182cf3618310819c4f326874
Xi LIU <xi.liu@u.nus.edu>
2024-08-06 15:28:51 +0800

    local

--- mk_dir.py
+++ mk_dir.py
@@ -8,7 +8,7 @@ TVals=[0.5,1,2]
 rowNum=0
 inParamFileName="./V_inv_12_6Params.csv"
 
-unitCellNum=2
+unitCellNum=5
 
 inDf=pd.read_csv(inParamFileName)
 oneRow=inDf.iloc[rowNum,:]
@@ -73,7 +73,7 @@ def contents_to_conf(k):
         "\n",
         "observable_name=U_dist\n",
         "\n",
-        "effective_data_num_required=2000\n",
+        "effective_data_num_required=3000\n",
         "\n",
         "loop_to_write=10000\n",
         "\n",
