74845ec5e2b5055ebc6b850c153769ebebd83a65
Xi LIU <xi.liu@u.nus.edu>
2024-08-08 14:42:07 +0800

    local

--- mk_dir.py
+++ mk_dir.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 #This script creates directories and conf files for mc
-TVals=[0.5,1,2,3,4,5,6,7,8]
+TVals=[1.5,2.5,3.5,4,4.5]
 rowNum=0
 inParamFileName="./V_inv_12_6Params.csv"
 
