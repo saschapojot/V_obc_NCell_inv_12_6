2d29464c52eac757bbf13622bac268a25c2232bc
Xi LIU <xi.liu@u.nus.edu>
2024-08-13 15:49:08 +0800

    loca

--- print_pkl.py
+++ print_pkl.py
@@ -4,17 +4,17 @@ import pandas as pd
 
 #this script prints part of an array
 
-inFile="./dataAllUnitCell10/row0/T0.5/U_dist_dataFiles/loopStart182000000loopEnd182999999.U_dist.pkl"
+inFile="./dataAllUnitCell10/row0/T2/U_dist_dataFiles/U/loopStart9100000loopEnd10099999.U.pkl"
 N=10
 
 with open(inFile,"rb") as fptr:
     arr=pickle.load(fptr)
 
-arr=np.reshape(arr,(-1,2*N+1))
+# arr=np.reshape(arr,(-1,2*N+1))
 
 outCsvName="./show.csv"
 
-part=arr[-20:,:]
+part=arr[-20:]
 
 df=pd.DataFrame(part)
 
